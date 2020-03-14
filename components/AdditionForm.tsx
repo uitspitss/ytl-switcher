0e9eb2d9bf0a9d6e72e6f1db0fc0163ee3a4dd3b
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-03-15 03:40:49 +0900

    Change AdditionForm

--- components/AdditionForm.tsx
+++ components/AdditionForm.tsx
@@ -1,10 +1,35 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import Modal from '@material-ui/core/Modal';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import styled from '@emotion/styled';
 import { StoreContext } from '../store';
 import { ADD_VIDEO } from '../actions';
 
+const StyledModal = styled(Modal)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledPaper = styled(Paper)`
+  height: 30vh;
+  width: 60vh;
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
 const AdditionForm: FC = () => {
   const { dispatch } = useContext(StoreContext);
+  const [open, setOpen] = useState(true);
 
   const { register, handleSubmit } = useForm();
   const onSubmit = (data: any) => {
@@ -14,12 +39,33 @@ const AdditionForm: FC = () => {
         payload: { channelId: data.channelId },
       });
   };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="channelId" ref={register({ required: true })} />
-      <input type="submit" value="追加する" />
-    </form>
+    <>
+      <Button onClick={handleOpen} variant="contained" color="secondary">
+        ライブ配信を追加する
+      </Button>
+      <StyledModal open={open} onClose={handleClose}>
+        <StyledPaper>
+          <StyledForm onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              name="channelId"
+              inputRef={register({ required: true })}
+              label="ライブ配信ID"
+            />
+            <Button type="submit" variant="contained" color="primary">
+              追加する
+            </Button>
+          </StyledForm>
+        </StyledPaper>
+      </StyledModal>
+    </>
   );
 };
 
