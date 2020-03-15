cd720ce985c34b1594a1df743d51a9cdd5b621bb
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-03-15 14:13:09 +0900

    Fix styles

--- shared/styles.tsx
+++ shared/styles.tsx
@@ -1,5 +1,5 @@
-import { keyframes, css, Global } from '@emotion/react';
-import styled from '@emotion/styled';
+import React from 'react';
+import { css, Global } from '@emotion/react';
 
 export const globalStyles = (
   <Global
@@ -27,41 +27,4 @@ export const basicStyles = css`
   transition: all 0.1s linear;
   margin: 3rem 0;
   padding: 1rem 0.5rem;
-`;
-
-export const hoverStyles = css`
-  &:hover {
-    color: white;
-    background-color: lightgray;
-    border-color: aqua;
-    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
-  }
-`;
-export const bounce = keyframes`
-  from {
-    transform: scale(1.01);
-  }
-  to {
-    transform: scale(0.99);
-  }
-`;
-
-export const Basic = styled('div')`
-  ${basicStyles};
-`;
-
-export const Combined = styled('div')`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-`;
-export const Animated = styled('div')`
-  ${basicStyles};
-  ${hoverStyles};
-  & code {
-    background-color: linen;
-  }
-  animation: ${props => props.animation} 0.2s infinite ease-in-out alternate;
 `;
