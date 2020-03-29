bca95fe18cc32c765c6fc9d1250ef68fcf872fc0
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-03-29 20:58:28 +0900

    Fix head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,6 +25,7 @@ export default class MyApp extends App {
             name="viewport"
             content="minimum-scale=1, initial-scale=1, width=device-width"
           />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
