5662a7dec24fe34a6b98fbcfe396df1db8610751
uitspitss <3781626+uitspitss@users.noreply.github.com>
2020-04-29 12:17:49 +0900

    Update forms

--- src/components/AdditionForm.tsx
+++ src/components/AdditionForm.tsx
@@ -1,52 +1,21 @@
 import React, { FC, useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import styled from '@emotion/styled';
+import BaseForm, { FormData } from './BaseForm';
 import { StoreContext } from '../store';
 import { ADD_VIDEO } from '../actions';
-
-const StyledForm = styled.form`
-  margin: 10px;
-`;
-
-const StyledTextField = styled(TextField)`
-  margin: 0 5px;
-`;
-
-type FormData = {
-  videoId: string;
-  channelId: string;
-};
 
 const AdditionForm: FC = () => {
   const { dispatch } = useContext(StoreContext);
 
-  const { register, handleSubmit, setValue } = useForm<FormData>();
-  const onSubmit = handleSubmit(({ videoId, channelId }) => {
-    if (videoId || channelId) {
-      dispatch({
-        type: ADD_VIDEO,
-        payload: { videoId, channelId },
-      });
-      setValue('videoId', '');
-      setValue('channelId', '');
-    }
-  });
+  const submit = ({ vid, cid }: FormData) => {
+    dispatch({
+      type: ADD_VIDEO,
+      payload: { videoId: vid, channelId: cid },
+    });
+  };
 
-  return (
-    <StyledForm onSubmit={onSubmit}>
-      <StyledTextField name="videoId" inputRef={register} label="Live ID" />
-      <StyledTextField
-        name="channelId"
-        inputRef={register}
-        label="Channel ID"
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Add
-      </Button>
-    </StyledForm>
-  );
+  const props = { videoId: '', channelId: '', btnLabel: 'Add', submit };
+
+  return <BaseForm {...props} />;
 };
 
 export default AdditionForm;
--- src/components/DeletionForm.tsx
+++ src/components/DeletionForm.tsx
@@ -1,60 +1,26 @@
 import React, { FC, useContext } from 'react';
-import { useForm } from 'react-hook-form';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import styled from '@emotion/styled';
+import BaseForm, { FormData } from './BaseForm';
 import { StoreContext } from '../store';
 import { DELETE_VIDEO } from '../actions';
-
-const StyledForm = styled.form`
-  margin: 10px;
-`;
-
-const StyledTextField = styled(TextField)`
-  margin: 0 5px;
-`;
 
 type Props = {
   videoId: string;
   channelId: string;
 };
 
-type FormData = {
-  vid: string;
-};
-
 const DeletionForm: FC<Props> = ({ videoId, channelId }) => {
   const { dispatch } = useContext(StoreContext);
 
-  const { register, handleSubmit } = useForm<FormData>();
-  const onSubmit = handleSubmit(({ vid }) => {
+  const submit = ({ vid }: FormData) => {
     dispatch({
       type: DELETE_VIDEO,
       payload: { videoId: vid },
     });
-  });
+  };
 
-  return (
-    <StyledForm onSubmit={onSubmit}>
-      <StyledTextField
-        name="vid"
-        inputRef={register}
-        defaultValue={videoId}
-        label="Live ID"
-        InputProps={{ readOnly: true }}
-      />
-      <StyledTextField
-        name="cid"
-        inputRef={register}
-        defaultValue={channelId}
-        label="Channel ID"
-        InputProps={{ readOnly: true }}
-      />
-      <Button type="submit" variant="outlined" color="primary">
-        Delete
-      </Button>
-    </StyledForm>
-  );
+  const props = { videoId, channelId, btnLabel: 'Delete', submit };
+
+  return <BaseForm {...props} />;
 };
 
 export default DeletionForm;
